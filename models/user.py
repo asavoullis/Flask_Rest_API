a843fc605d385289b0f7d3c708f54cd9f261a3a7
Charilaos Alkiviades Savoullis <casavoullis@gmail.com>
2024-03-28 21:11:00 +0000

    added another column in user model - table, added more instructions on how to use, and updated the requirements file

--- models/user.py
+++ models/user.py
@@ -4,16 +4,22 @@ from datetime import datetime
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
+
     first_name = db.Column(db.String(50), nullable=False)
     surname = db.Column(db.String(50), nullable=False)
+
     address = db.Column(db.String(255), nullable=False)
     telephone_number = db.Column(db.String(20), nullable=False)
     country_code = db.Column(db.String(5), nullable=False)
     post_code = db.Column(db.String(20), nullable=False)
+
     email = db.Column(db.String(120), nullable=False)
+
     is_active = db.Column(db.Boolean, default=True)
+
     date_of_birth = db.Column(db.Date, nullable=False)
     accounts = db.relationship('Account', backref='user', lazy='dynamic')
+    registration_date = db.Column(db.DateTime, default=db.func.current_timestamp())
 
     def __repr__(self):
         return f'<User {self.username}>'
@@ -30,5 +36,6 @@ class User(db.Model):
             'post_code': self.post_code,
             'email': self.email,
             'is_active': self.is_active,
-            'date_of_birth': self.date_of_birth.strftime('%d-%m-%Y')
+            'date_of_birth': self.date_of_birth.strftime('%d-%m-%Y'),
+            'registration_date': self.registration_date.strftime('%d-%m-%Y')
         }
